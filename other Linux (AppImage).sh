cdd793348f8587f579956a1cbcfe5a73b54a27f6
raniel laguna <avraniel@gmail.com>
2026-02-21 09:32:52 +0100

    Update other Linux (AppImage).sh
    edit

--- other Linux (AppImage).sh
+++ other Linux (AppImage).sh
@@ -1,4 +1,4 @@
 # Download AppImage
-wget https://github.com/yourusername/audio-converter-pro/releases/latest/download/AudioConverterPro-x86_64.AppImage
+wget https://github.com/avraniel/audio-converter-pro/releases/latest/download/AudioConverterPro-x86_64.AppImage
 chmod +x AudioConverterPro-x86_64.AppImage
 ./AudioConverterPro-x86_64.AppImage
