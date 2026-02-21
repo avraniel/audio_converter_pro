e4134299ac975dca90e475283efb5d00122d0e7e
raniel laguna <avraniel@gmail.com>
2026-02-21 09:34:16 +0100

    Update setup (for pip installation).py
    edit

--- setup (for pip installation).py
+++ setup (for pip installation).py
@@ -11,7 +11,7 @@ setup(
     description="Professional audio converter supporting multiple formats",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/audio-converter-pro",
+    url="https://github.com/avraniel/audio-converter-pro",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
