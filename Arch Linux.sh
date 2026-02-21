dae20dd935ee892fc4a3aec392a9dde1c71262d3
raniel laguna <avraniel@gmail.com>
2026-02-21 09:31:09 +0100

    Update Arch Linux.sh
    edit

--- Arch Linux.sh
+++ Arch Linux.sh
@@ -1,4 +1,4 @@
 # From AUR
 yay -S audio-converter-pro
 # Or manual install
-curl -sSL https://raw.githubusercontent.com/yourusername/audio-converter-pro/main/scripts/install_arch.sh | bash
+curl -sSL https://raw.githubusercontent.com/avraniel/audio-converter-pro/main/scripts/install_arch.sh | bash
